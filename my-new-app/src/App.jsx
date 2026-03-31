1cda98b0561a2950818497823df9c47586130546
Mahmoda-Akte <snehakter022@gmail.com>
2026-03-31 09:52:37 -0700

    update data

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,7 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
+
 import { Suspense, useState } from 'react'
 import './App.css'
 import Banner from './Component/Homepage/Banner'
